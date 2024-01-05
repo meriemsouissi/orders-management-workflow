a78094e690baacf6e634b16c316b7b1c26ffe63a
Meriem Souissi <souissiimeriem@gmail.com>
2024-01-05 16:10:32 +0000

    order management

---
+++ workflow-ui-module/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ordersmngmtnsp/workflowuimodule/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ workflow-ui-module/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ordersmngmtnsp/workflow-ui-module/test/unit/controller/orderApprovalForm.controller"
+], function () {
+	"use strict";
+});
